1aaeec20eaf9f36583c89090a99dbf1de51a65ae
lais santos <laissdj49@gmail.com>
2023-11-19 19:28:24 +0100

    layout creating

--- app/src/main/java/com/example/gameofthrones/view/GOTView.kt
+++ app/src/main/java/com/example/gameofthrones/view/GOTView.kt
@@ -5,13 +5,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.gameofthrones.domain.GOTPresentation
 
@@ -24,6 +34,7 @@ fun Characters(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.onBackground)
+            .padding(16.dp)
 
     ) {
         AsyncImage(
@@ -32,25 +43,53 @@ fun Characters(
             contentScale = ContentScale.FillHeight,
             contentDescription = ""
         )
-
         Column(
-            modifier = Modifier,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center
 
         ) {
             Text(
-                text = character.fullName,
-                color = Color.White,
+                text = character.firstName,
+                color = Color.Red,
+                modifier = modifier.shadow(elevation = 20.dp, spotColor = Color.Red),
+                style = TextStyle(
+                    fontFamily = FontFamily.Cursive,
+                    textDecoration = TextDecoration.Underline
+                ),
+                fontWeight = FontWeight.Bold,
+                fontSize = 50.sp,
+                maxLines = 1
+            )
+            Text(
+                text = character.lastName,
+                modifier = modifier.padding(start = 20.dp),
+                style = TextStyle(
+                    fontFamily = FontFamily.Cursive,
+                    textDecoration = TextDecoration.Underline
+                ),
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontSize = 50.sp,
                 maxLines = 1
             )
             Text(
                 text = character.title,
-                color = Color.White
+                modifier = modifier,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(fontStyle = FontStyle.Italic),
+                fontSize = 20.sp,
+                maxLines = 2
             )
             Text(
                 text = character.family,
-                color = Color.White
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(fontStyle = FontStyle.Italic),
+                fontSize = 20.sp,
             )
         }
     }
